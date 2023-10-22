a368e1608f72a553edbf5d66d369692b1a51c1d1
yantheLousada <yanthe96@hotmail.com>
2023-10-22 12:10:28 -0300

    contador java

---
+++ Contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
